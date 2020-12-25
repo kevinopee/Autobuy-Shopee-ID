13e5e1357f5788e9bb4f344bc9cab79ad6e44f85
Kevin Ope <41668520+kevinopee@users.noreply.github.com>
2020-12-26 02:16:29 +0700

    Update shopee_bot.py

--- Shopee Autobuy/shopee_bot.py
+++ Shopee Autobuy/shopee_bot.py
@@ -15,15 +15,9 @@ from fake_useragent import UserAgent
 UC.TARGET_VERSION = 87 # You can change your chrome version target
 
 options = UC.ChromeOptions()
-hl = input("Headless / Normal")
-if hl == "Headless":
-    options.headless = True
-    options.add_argument("--headless")
-else:
-    options.headless = False
-    options.add_argument("start-maximized")
+options.headless = False
 # Add proxy to become more invisible
-server = input("Proxy / Socks5 / None : ")
+server = input("Proxy / Socks5 / Nope : ")
 if server.lower == "proxy":
     proxy = input("Masukan proxy : ")
     options.add_argument(f'--proxy-server={proxy}')
@@ -51,35 +45,38 @@ def authors():
 
 def load_cookies():
     browser.get("https://shopee.co.id")
-    cookies = pickle.load(open("cookies.pkl", "rb"))
-    for cookie in cookies:
-        browser.add_cookie(cookie)
+    try:
+        cookies = pickle.load(open("cookies.pkl", "rb"))
+        for cookie in cookies:
+            browser.add_cookie(cookie)
+    except NoSuchCookieException as e:
+        print(e)
 
 def tombol_beli():
     try:
         beli = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((
-            By.XPATH, '//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[5]/div/div/button[2]')))
+                By.XPATH, '//*[@id="main"]/div/div[2]/div[2]/div[2]/div[2]/div[3]/div/div[5]/div/div/button[2]')))
         browser.execute_script("arguments[0].click();", beli)
         print("- Barang terbeli!")
         print(datetime.datetime.now().microsecond)
         # iframe = WebDriverWait(browser, 60).until(EC.frame_to_be_available_and_switch_to_it((
         #        By.XPATH, '# //*[@id="main"]/div/div[2]/div[2]')))
         checkout = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((
-            By.XPATH, '//*[@id="main"]/div/div[2]/div[2]/div[3]/div[2]/div[7]/div[5]/button/span')))
+                By.XPATH, '//*[@id="main"]/div/div[2]/div[2]/div[3]/div[2]/div[7]/div[5]/button/span')))
         browser.execute_script("arguments[0].click();", checkout)
         print("- Barang tercheckout!")
         print(datetime.datetime.now().microsecond)
         pesanan = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((
-            By.XPATH, '//*[@id="main"]/div/div[2]/div[3]/div[4]/div[2]/div[7]/button')))
+                By.XPATH, '//*[@id="main"]/div/div[2]/div[3]/div[4]/div[2]/div[7]/button')))
         browser.execute_script("arguments[0].click();", pesanan)
         print("- Barang terpesan!")
         print(datetime.datetime.now().microsecond)
         bayar = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((
-            By.ID, 'pay-button'))).click()
+                By.ID, 'pay-button'))).click()
         browser.execute_script("arguments[0].click();", bayar)
         print("- Barang terbayar!")
         pin_shopee = WebDriverWait(browser, 60).until(EC.element_to_be_clickable((
-            By.XPATH, '//*[@id="pin-popup"]/div[1]/div[3]/div[1]')))
+                By.XPATH, '//*[@id="pin-popup"]/div[1]/div[3]/div[1]')))
         browser.execute_script("arguments[0].click();", pin_shopee)
         pin_shopee.send_keys(ac.pin_number)
     except NoSuchElementException as e:
@@ -93,11 +90,10 @@ def main():
     # Input product link
     link_produk = input("Masukan link produk : ")
     browser.get(link_produk)
-    # Input your flash sale minute
     menit = int(input("Masukan menit untuk memulai beli : "))
 
     # This is my masterpiece logic piece of shit
-    # Ini countdown buat nentuin menit beli
+    # Ini countdown buat nentuin menit beli 
     while minute != menit:
         minute = datetime.datetime.now().minute
 
